2e2d060fc307a49222e9d4992d22885b52ce4bfd
최정윤 <jes2ngyun@gmail.com>
2026-07-03 11:23:19 +0900

    chore: 일정 과제 기본 의존성 및 패키지 구조 설정

---
+++ src/test/java/com/sparta/basicschedule/BasicScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.basicschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
